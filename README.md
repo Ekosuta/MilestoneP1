c8a15095f8b61f57ac097569210b16932548c1f7
Elvin Kosuta <elvinkousta6@gmail.com>
2023-04-04 19:51:03 -0700

    final check

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MilestoneP1
+
+This project is supposed to an immitation of the popular pong game. Two players battle the ball back and forth until one is unable to reach the ball in time, resulting in a point for the opposing player.
+
+The game is simple: Player 1 is on the left and uses keys "W" to move up, and "S" to move down, while player 2 is on the right and uses the up and down arrow keys.
+
+To restart the game, press the restart button at the bottom. This will reset the score as well as the balls position!
+
+Have fun!
--- script.js
+++ script.js
@@ -1,50 +1,36 @@
-/*import Ball from './Ball.js'
-
-let ball = new Ball(document.getElementById("ball"))
-
-let lastTime
-function refresh(time){
-    if(lastTime != null){
-        let delta = time - lastTime
-        ball.refresh(delta)
-    }
-    lastTime = time
-    window.requestAnimationFrame(refresh)
-}
-
-window.requestAnimationFrame(refresh)*/
-const gamePlatform = document.querySelector("#gamePlatform")
-const ctx = gamePlatform.getContext("2d")
-const ball = document.getElementById("ball")
+const gamePlatform = document.querySelector("#gamePlatform");
+const ctx = gamePlatform.getContext("2d");
+const score = document.querySelector("#score");
+const resetButton = document.querySelector("#resetButton");
+const gameWidth = gamePlatform.width;
+const gameHeight = gamePlatform.height;
+const gameBackground = "forestgreen";
+const racket1Color = "white";
+const racket2Color = "white";
+const racketBorder = "darkgreen";
+const ballColor = "blue";
+const ballBorder = "darkblue";
+const ballRadius = 12.5;
 const racketSpeed = 50;
-const gameWidth = window.innerWidth
-const gameHeight = window.innerHeight
-const resetButton = document.getElementById("resetButton")
+let intervalID;
 let ballSpeed = 1;
 let ballX = gameWidth / 2;
 let ballY = gameHeight / 2;
 let ballXDirection = 0;
 let ballYDirection = 0;
 let player1Score = 0;
-let intervalID;
 let player2Score = 0;
-let player1Racket = document.getElementById("player1-racket")
-let player2Racket = document.getElementById("player2-racket")
-let racketHeight = player1Racket.offsetHeight
-let racket1Color = document.querySelector("--foreground-color")
-let racket2Color = document.querySelector("--foreground-color")
-let racketBorder = "green";
 let racket1 = {
     width: 25,
     height: 100,
-    x: 50,
-    y: gameHeight / 2
+    x: 0,
+    y: 0
 }
 let racket2 = {
     width: 25,
     height: 100,
-    x: gameWidth - 75,
-    y: gameHeight / 2
+    x: gameWidth - 25,
+    y: gameHeight - 100
 }
 
 window.addEventListener("keydown", changeDirection);
@@ -53,68 +39,162 @@ resetButton.addEventListener("click", resetGame);
 gameStart();
 
 function gameStart() {
-    create
+    makeBall();
+    refresh();
 };
 
 function refresh() {
     intervalID = setTimeout(() => {
+        clearPlatform();
+        insertRacket();
         moveBall();
+        insertBall(ballX, ballY);
         checkCollision();
         refresh();
-        insertRacket();
     }, 10)
 };
 
-function insertRacket() {
-    ctx.strokeStyle = racketBorder
-
-    ctx.fillStyle = racket1Color
-    ctx.fillRect(racket1.y, racket1.x, racket1, width, racket1.height)
-    ctx.strokeRect(racket1.y, racket1.x, racket1, width, racket1.height)
-
-    ctx.fillStyle = racket2Color
-    ctx.fillRect(racket2.y, racket2.x, racket2, width, racket2.height)
-    ctx.strokeRect(racket2.y, racket2.x, racket2, width, racket2.height)
+function clearPlatform(){
+    ctx.fillStyle = gameBackground;
+    ctx.fillRect(0, 0, gameWidth, gameHeight);
 };
 
-function makeBall() { };
+function insertRacket() {
+    ctx.strokeStyle = racketBorder;
 
-function moveBall() { };
+    ctx.fillStyle = racket2Color;
+    ctx.fillRect(racket2.y, racket2.x, racket2.width, racket2.height);
+    ctx.strokeRect(racket2.y, racket2.x, racket2.width, racket2.height);
+    ctx.fillStyle = racket1Color;
+    ctx.fillRect(racket1.y, racket1.x, racket1.width, racket1.height);
+    ctx.strokeRect(racket1.y, racket1.x, racket1.width, racket1.height);
 
-function insertBall() { };
+};
 
-function checkCollision() { };
+function makeBall() {
+    ballSpeed = 1;
+    if(Math.round(Math.random()) == 1){
+        ballXDirection = 1;
+    }
+    else{
+        ballXDirection = -1;
+    }
+    if(Math.round(Math.random()) == 1){
+        ballYDirection = 1;
+    }
+    else{
+        ballYDirection = -1;
+    }
+    ballX = gameWidth / 2;
+    ballY = gameHeight / 2;
+    insertBall(ballX, ballY);
+};
 
-function changeDirection(event) {
-    const keyPressed = event.keyCode
-    const racket1Up = 87
-    const racket1Down = 83
-    const racket2Up = 38
-    const racket2Down = 40
-    switch (keyPressed) {
-        case (racket1Up):
-            if (player1Racket.y > 0) {
-                player1Racket.y -= racketSpeed
-            }
-            break
-        case (racket1Down):
-            if (player1Racket.y < gameHeight - racketHeight) {
-                player1Racket.y += racketSpeed
-            }
-            break
-        case (racket2Up):
-            if (player2Racket.y > 0) {
-                player2Racket.y -= racketSpeed
-            }
-            break
-        case (racket2Down):
-            if (player2Racket.y < gameHeight - racketHeight) {
-                player2Racket.y += racketSpeed
-            }
-            break
+function moveBall() {
+    ballX += (ballSpeed * ballXDirection)
+    ballY += (ballSpeed * ballYDirection)
+};
+
+function insertBall(ballX, ballY) {
+    ctx.fillStyle = ballColor;
+    ctx.strokeStyle = ballBorder;
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+};
+
+function checkCollision() {
+    if(ballY <= 0 + ballRadius){
+        ballYDirection *= -1;
+    }
+    if(ballY >+ gameHeight - ballRadius){
+        ballYDirection *= -1;
+    }
+    if(ballX <= 0){
+        player2Score+=1;
+        updateScore();
+        makeBall();
+        return;
+    }
+    if(ballX >= gameWidth - ballRadius){
+        player1Score+=1;
+        updateScore();
+        makeBall();
+        return;
+    }
+    if(ballX <= (racket1.x + racket1.width + ballRadius)){
+        if(ballY > racket1.y && ballY < racket1.y + racket1.height){
+            ballX = (racket1.x + racket1.width) + ballRadius;
+            ballXDirection *= -1;
+            ballSpeed += 1;
+        }
+    }
+    if(ballX >= (racket2.x - ballRadius)){
+        if(ballY > racket2.y && ballY < racket2.y + racket2.height){
+            ballX = racket2.x - ballRadius;
+            ballXDirection *= -1;
+            ballSpeed += 1;
+        }
     }
 };
 
-function updateScore() { };
+function changeDirection(event) {
+    const keyPressed = event.keyCode;
+    const racket1Up = 87;
+    const racket1Down = 83;
+    const racket2Up = 38;
+    const racket2Down = 40;
+    switch (keyPressed) {
+        case (racket1Up):
+            if (racket1.x > 0) {
+                racket1.x -= racketSpeed;
+            }
+            break;
+        case (racket1Down):
+            if (racket1.x < gameHeight - racket1.height) {
+                racket1.x += racketSpeed;
+            }
+            break;
+        case (racket2Up):
+            if (racket2.x > 0) {
+                racket2.x -= racketSpeed;
+            }
+            break;
+        case (racket2Down):
+            if (racket2.x < gameHeight - racket2.height) {
+                racket2.x += racketSpeed;
+            }
+            break;
+    }
+};
 
-function resetGame() { };
+function updateScore() {
+    score.textContent = `${player1Score} : ${player2Score}`
+};
+
+function resetGame() {
+    player1Score = 0;
+    player2Score = 0;
+    racket1 = {
+        width: 25,
+        height: 100,
+        x: 0,
+        y: 0
+    }
+    racket2 = {
+        width: 25,
+        height: 100,
+        x: gameWidth - 25,
+        y: gameHeight - 100
+    }
+    ballSpeed = 1;
+    ballX = 0;
+    ballY = 0;
+    ballXDirection = 0;
+    ballYDirection = 0;
+    updateScore();
+    clearInterval(intervalID);
+    gameStart();
+};
